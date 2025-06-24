9d79f53b7a484f9f3339e71e87ae26d657088aa5
Raylla-sam <rayllasampaio51@gmail.com>
2025-06-24 12:51:06 -0300

    Adiciona e corrige novas rotas do app

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,15 +3,19 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Home from '../screens/Home';
 import Rotina from '../screens/Rotina';
 import Emocional from '../screens/Emocional';
+import Respiracao from '../screens/Emocional/Respiracao';
+import Meditacao from '../screens/Emocional/Meditacao';
+import BottomTabs from './BottomTabs';
 
 const Stack = createNativeStackNavigator();
 
 export default function AppNavigator() {
   return (
-    <Stack.Navigator initialRouteName="Home">
-      <Stack.Screen name="Home" component={Home} />
-      <Stack.Screen name="Rotina" component={Rotina} />
-      <Stack.Screen name="Emocional" component={Emocional} />
+    <Stack.Navigator initialRouteName="MainTabs">
+      <Stack.Screen name="MainTabs" component={BottomTabs} options={{ title: 'Inicio', headerShown: false }}/>
+
+      <Stack.Screen name="Respiracao" component={Respiracao} />
+      <Stack.Screen name="Meditacao" component={Meditacao} />
     </Stack.Navigator>
   );
 }
